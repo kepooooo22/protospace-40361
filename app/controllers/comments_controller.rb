ad0443b168a24e5583bd4b65ba3977908bdc1f3a
kepooooo22 <kkk0220131kkk@ezweb.ne.jp>
2024-03-01 01:57:43 +0900

    修正２

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,11 +12,6 @@ class CommentsController < ApplicationController
     end
   end
 
-  def show
-    @comment = Comment.new
-    @comments = @prototype.comments.includes(:user)
-  end
-
   private
   def comment_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -22,7 +22,7 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
+    @comment = @prototype.comments
     @comment = Comment.new
   end
 
@@ -30,7 +30,6 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    @prototype = Prototype.find(params[:id]) 
     if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
     else
